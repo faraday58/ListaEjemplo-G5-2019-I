8845461fa1c7844db6303d9d5da2c818e6503484
faraday58 <jarodriguezvera@gmail.com>
2018-10-03 14:12:50 -0500

    Agregar archivos de proyecto.

---
+++ ListaEjemplo-G5-2019-I/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEjemplo_G5_2019_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
