5901271c07101e10dc44d4ee9057699c9f780af0
faraday58 <jarodriguezvera@gmail.com>
2018-10-03 14:55:13 -0500

    Ejemplo de Listas

--- ListaEjemplo-G5-2019-I/Program.cs
+++ ListaEjemplo-G5-2019-I/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Pokemones;
 
 namespace ListaEjemplo_G5_2019_I
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<Pokemon> pokemones = new List<Pokemon>();
+
+            Pokemon pokemon1 = new Pokemon("Bulbasaur", "planta", 5, "fuego", true);
+            Pokemon pokemon2 = new Pokemon("Charmander", "Fuego", 5, "Agua", true);
+            Pokemon pokemon3 = new Pokemon("Mew", "Psiquico", 100, "Siniestro", true);
+
+            pokemones.Add(pokemon1);
+            pokemones.Add(pokemon2);
+            pokemones.Add(pokemon3);
+   
+            foreach (Pokemon pkmn in pokemones)
+            {
+                Console.WriteLine("Tu pokemon : {0} Tipo: {1} nivel: {2} Debilidad: {3} sexo: {4}  ", pkmn.Nombre, pkmn.Tipo, pkmn.Nivel, pkmn.Debilidad, pkmn.Sexo);
+            }
+
+
+            Console.WriteLine("\n");
+            pokemones.Remove(pokemon1);
+
+            foreach (Pokemon pkmn in pokemones)
+            {
+                Console.WriteLine("Tu pokemon : {0} Tipo: {1} nivel: {2} Debilidad: {3} sexo: {4}  ", pkmn.Nombre, pkmn.Tipo, pkmn.Nivel, pkmn.Debilidad, pkmn.Sexo);
+            }
+
+            Console.WriteLine("Qué pokemon deseas eliminar?");
+            string nombre= Console.ReadLine();
+
+            for(int i=0; i<pokemones.Count;i++)
+            {
+                if(nombre==pokemones[i].Nombre)
+                {
+                    pokemones.Remove(pokemones[i]);
+                }
+            }
+
+            foreach (Pokemon pkmn in pokemones)
+            {
+                Console.WriteLine("\nTu pokemon : {0} Tipo: {1} nivel: {2} Debilidad: {3} sexo: {4}  ", pkmn.Nombre, pkmn.Tipo, pkmn.Nivel, pkmn.Debilidad, pkmn.Sexo);
+            }
+
+
         }
     }
 }
---
+++ Pokemones/Class1.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Pokemones
+{
+    public class Pokemon
+    {
+
+        #region Atributos
+        private string nombre;
+        private string tipo;
+        private byte nivel;
+        private string debilidad;
+        private bool sexo;
+        #endregion
+
+        #region Propiedades
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
+        public byte Nivel { get => nivel; set => nivel = value; }
+        public string Debilidad { get => debilidad; set => debilidad = value; }
+        public bool Sexo { get => sexo; set => sexo = value; }
+        #endregion
+
+        #region Constructor
+        public Pokemon(string nombre, string tipo, byte nivel, string debilidad, bool sexo)
+        {
+            Nombre = nombre;
+            Tipo = tipo;
+            Nivel = nivel;
+            Debilidad = debilidad;
+            Sexo = sexo;
+        }
+        #endregion
+
+    }
+}
